2919561be9198daff79b72771067a6b8f213db14
meg <megumi_add@yahoo.co.jp>
2024-04-10 15:28:19 +0900

    2024-04-10 15:28

---
+++ java_basic/src/java_basic/trainning1/ShowTriangle.java
@@ -0,0 +1,13 @@
+package java_basic.trainning1;
+
+public class ShowTriangle {
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+
+}
